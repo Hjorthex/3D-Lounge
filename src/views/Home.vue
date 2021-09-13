69fe12d74897b9e34a8d541fe76499dc9293d277
Martin Windsor <martinwindsor1@gmail.com>
2021-09-13 08:49:19 +0200

    Basic chat for MVP

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Comments />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Comments from "@/components/troubleshoot/troubleshoot-issues/Comments.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Comments,
+  },
+};
 </script>
