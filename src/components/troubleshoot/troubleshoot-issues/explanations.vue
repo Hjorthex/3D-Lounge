69421b53e5226aefe81740cc1abe271b9255a77e
Hjorthex <74530828+Hjorthex@users.noreply.github.com>
2021-09-14 10:40:39 +0200

    asndfkodsa

--- src/components/troubleshoot/troubleshoot-issues/explanations.vue
+++ src/components/troubleshoot/troubleshoot-issues/explanations.vue
@@ -13,6 +13,19 @@
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore qui, omnis odio eum laborum quo minima totam culpa amet cum sint consequuntur repellendus modi sed? Dolore recusandae incidunt pariatur nisi perferendis dicta corporis optio facilis at laudantium? Asperiores fugit blanditiis doloremque suscipit, hic qui libero, et provident veniam dicta dolor.
             </p>
         </div>
+        <div class="explanations__problem-container">
+            <div class="explanations__caption-layout">
+                <div class="explanations__question-icon">
+                    <p>
+                        ?
+                    </p>
+                </div>
+                <h2>What is it?</h2>
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore qui, omnis odio eum laborum quo minima totam culpa amet cum sint consequuntur repellendus modi sed? Dolore recusandae incidunt pariatur nisi perferendis dicta corporis optio facilis at laudantium? Asperiores fugit blanditiis doloremque suscipit, hic qui libero, et provident veniam dicta dolor.
+            </p>
+        </div>
     </div>
 </template>
 
